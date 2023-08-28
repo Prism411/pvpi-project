40b364074a9eff555f9c1f09a49405b684dca0ad
Prism411 <jaderlouis@proton.me>
2023-08-28 19:42:18 -0300

    working on IA indentifier

--- model.py
+++ model.py
@@ -1,39 +1,40 @@
 import cv2
 import numpy as np
-def detect_inconsistent_texture(image):
-    # Carregar a imagem em escala de cinza
-    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
 
-    # Aplicar o filtro de detecção de borda (por exemplo, Canny)
-    edges = cv2.Canny(image, threshold, threshold * 2)
+def detect_inconsistent_texture(image, threshold=5):
+    # Converter a imagem em escala de cinza
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Aplicar o filtro de detecção de borda (Canny)
+    edges = cv2.Canny(gray_image, threshold, threshold * 2)
 
     # Calcular a porcentagem de pixels ativos nas bordas
     edge_percentage = np.sum(edges) / (image.shape[0] * image.shape[1]) * 100
-
+    print(edge_percentage)
     # Definir um limiar para detectar textura inconsistente
-    if edge_percentage > 10:  # Ajuste o limiar conforme necessário
-        return True
-    else:
-        return False
-
-def detect_repetitive_patterns(image_path, threshold=0.8):
-    # Carregar a imagem em escala de cinza
-    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-
-    # Aplicar detecção de borda (Canny)
-    edges = cv2.Canny(image, 50, 150)
-
-    # Calcular a porcentagem de pixels ativos nas bordas
-    edge_percentage = np.sum(edges) / (image.shape[0] * image.shape[1])
-
-    # Se a porcentagem for maior que o limiar, considerar como padrão repetitivo
     if edge_percentage > threshold:
         return True
     else:
         return False
 
-image = cv2.imread(image_path)
+def detect_repetitive_patterns(image, threshold=0.08):
+    # Converter a imagem em escala de cinza
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+    # Aplicar detecção de borda (Canny)
+    edges = cv2.Canny(gray_image, 50, 150)
+
+    # Calcular a porcentagem de pixels ativos nas bordas
+    edge_percentage = np.sum(edges) / (image.shape[0] * image.shape[1])
+
+    # Se a porcentagem for maior que o limiar, considerar como padrão repetitivo
+    print(edge_percentage)
+    if edge_percentage > threshold:
+        return True
+    else:
+        return False
+
+def detect_vibrant_colors(image, threshold=0.005):
     # Converter a imagem de BGR para HSV (matiz, saturação, valor)
     hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
@@ -46,17 +47,16 @@ image = cv2.imread(image_path)
 
     # Calcular a porcentagem de pixels vibrantes na imagem
     vibrant_percentage = np.sum(mask == 255) / (image.shape[0] * image.shape[1]) * 100
-
+    print(vibrant_percentage)
     # Se a porcentagem for maior que o limiar, considerar como uso intenso de cores vibrantes
     if vibrant_percentage > threshold:
         return True
     else:
         return False
 
-
 def main(image_path):
     # Carregar a imagem
-    image = carregar_imagem(image_path)
+    image = cv2.imread(image_path)
 
     # Realizar as detecções
     inconsistent_texture = detect_inconsistent_texture(image)
@@ -72,5 +72,5 @@ def main(image_path):
         print("Uso intenso de cores vivas e contrastantes detectado.")
 
 # Caminho da imagem a ser analisada
-caminho_da_imagem = "caminho/para/sua/imagem.png"
+caminho_da_imagem = "images_data_base\\real3.jpg"
 main(caminho_da_imagem)
