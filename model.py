74429d1c6ba0cc5424bd8c4e576bb43328d0cfd2
Prism411 <jaderlouis@proton.me>
2023-08-28 19:21:15 -0300

    adding images to project

--- model.py
+++ model.py
@@ -16,17 +16,43 @@ def detect_inconsistent_texture(image):
     else:
         return False
 
-def detect_repetitive_patterns(image):
-    return true
-    # Verificar padrões repetitivos
-    # Use técnicas de processamento de imagem para identificar padrões repetitivos,
-    # como transformada de Fourier, detecção de linhas, detecção de borda, etc.
+def detect_repetitive_patterns(image_path, threshold=0.8):
+    # Carregar a imagem em escala de cinza
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
 
-def detect_vibrant_colors(image):
-    return true
-    # Verificar uso intenso de cores vivas e contrastantes
-    # Analise a distribuição de cores na imagem e identifique áreas com cores
-    # vibrantes e alto contraste em relação ao fundo.
+    # Aplicar detecção de borda (Canny)
+    edges = cv2.Canny(image, 50, 150)
+
+    # Calcular a porcentagem de pixels ativos nas bordas
+    edge_percentage = np.sum(edges) / (image.shape[0] * image.shape[1])
+
+    # Se a porcentagem for maior que o limiar, considerar como padrão repetitivo
+    if edge_percentage > threshold:
+        return True
+    else:
+        return False
+
+image = cv2.imread(image_path)
+
+    # Converter a imagem de BGR para HSV (matiz, saturação, valor)
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    # Definir faixa de cores vibrantes (vermelho, amarelo, verde, azul)
+    lower_vibrant = np.array([0, 100, 100])
+    upper_vibrant = np.array([120, 255, 255])
+
+    # Criar uma máscara para cores vibrantes
+    mask = cv2.inRange(hsv_image, lower_vibrant, upper_vibrant)
+
+    # Calcular a porcentagem de pixels vibrantes na imagem
+    vibrant_percentage = np.sum(mask == 255) / (image.shape[0] * image.shape[1]) * 100
+
+    # Se a porcentagem for maior que o limiar, considerar como uso intenso de cores vibrantes
+    if vibrant_percentage > threshold:
+        return True
+    else:
+        return False
+
 
 def main(image_path):
     # Carregar a imagem
