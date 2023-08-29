3c8dabc7e30754648ff55ca54b7fb225a3c401f9
Prism411 <jaderlouis@proton.me>
2023-08-29 11:54:31 -0300

    calibration of parameters of IA flag

--- model.py
+++ model.py
@@ -1,23 +1,23 @@
 import cv2
 import numpy as np
 
-def detect_inconsistent_texture(image, threshold=5):
+def detect_inconsistent_texture(image, threshold=5000, limit=5):
     # Converter a imagem em escala de cinza
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # Aplicar o filtro de detecção de borda (Canny)
-    edges = cv2.Canny(gray_image, threshold, threshold * 2)
+    edges = cv2.Canny(gray_image, limit, limit * 2)
 
     # Calcular a porcentagem de pixels ativos nas bordas
     edge_percentage = np.sum(edges) / (image.shape[0] * image.shape[1]) * 100
     print(edge_percentage)
     # Definir um limiar para detectar textura inconsistente
-    if edge_percentage > threshold:
-        return True
+    if edge_percentage < threshold:
+        return True #da flag na IA.
     else:
         return False
 
-def detect_repetitive_patterns(image, threshold=0.08):
+def detect_repetitive_patterns(image, threshold=8):
     # Converter a imagem em escala de cinza
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
@@ -29,12 +29,12 @@ def detect_repetitive_patterns(image, threshold=0.08):
 
     # Se a porcentagem for maior que o limiar, considerar como padrão repetitivo
     print(edge_percentage)
-    if edge_percentage > threshold:
-        return True
+    if edge_percentage < threshold:
+        return True #da flag que ela pode ser gerada por ia
     else:
         return False
 
-def detect_vibrant_colors(image, threshold=0.005):
+def detect_vibrant_colors(image, threshold=17):
     # Converter a imagem de BGR para HSV (matiz, saturação, valor)
     hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
@@ -49,8 +49,8 @@ def detect_vibrant_colors(image, threshold=0.005):
     vibrant_percentage = np.sum(mask == 255) / (image.shape[0] * image.shape[1]) * 100
     print(vibrant_percentage)
     # Se a porcentagem for maior que o limiar, considerar como uso intenso de cores vibrantes
-    if vibrant_percentage > threshold:
-        return True
+    if vibrant_percentage > threshold or vibrant_percentage < 1.0:
+        return True #Posivo gerada por IA!!
     else:
         return False
 
@@ -66,11 +66,17 @@ def main(image_path):
     # Imprimir resultados
     if inconsistent_texture:
         print("Textura inconsistente detectada.")
-    if repetitive_patterns:
+
+
+    if repetitive_patterns: #FEITO CALIBRAGEM PARA IA
         print("Padrões repetitivos detectados.")
-    if vibrant_colors:
+        #adicionar peso de probabilidade que ela pode ser uma IA.
+
+
+    if vibrant_colors: #FEITO CALIBRAGEM PARA IA
         print("Uso intenso de cores vivas e contrastantes detectado.")
+        #adicionar peso de probabilidade que ela pode ser uma IA.
 
 # Caminho da imagem a ser analisada
-caminho_da_imagem = "images_data_base\\real3.jpg"
+caminho_da_imagem = "images_data_base\\ia.jpg"
 main(caminho_da_imagem)
